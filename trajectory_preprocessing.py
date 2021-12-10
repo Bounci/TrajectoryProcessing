ece5b3672a48329073497a87038a10c4758508c1
thinkbook <572135712@qq.com>
2021-12-10 14:45:10 +0800

    测试连接

---
+++ trajectory_preprocessing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# author: Bounci
+# time: 2021/12/10
+# description: 
+
+# 读取轨迹数据（单一csv文件）
+def read_csv_trajectory_file():
+    pass
+
+
+if __name__ == '__main__':
+    pass
